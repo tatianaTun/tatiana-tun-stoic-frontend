fa2b78765751d2332e04273158bf57885e949ee5
tatianaTun <tatianatun92@gmail.com>
2024-03-20 14:36:57 -0700

    Removed a line from App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
-import React, { useState, useEffect } from "react";
 import MainPage from "./pages/MainPage/MainPage";
 import ExercisePage from "./pages/ExercisePage/ExercisePage";
 
