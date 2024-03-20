cdf33f709047b553542f54a0470e5c1047debe21
tatianaTun <tatianatun92@gmail.com>
2024-03-20 14:27:31 -0700

    Hid openAI api key in .env file. Added random quote to the page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import MainPage from "./pages/MainPage/MainPage";
-import ExercisePage from "./pages/ExercisePage/ExercisePage";
+// import ExercisePage from "./pages/ExercisePage/ExercisePage";
 
 function App() {
 
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         {/* <Route path="/exercises/" element={<ExercisesPage />} /> */}
-        <Route path="/exercise/:id" element={<ExercisePage />} />
+        {/* <Route path="/exercise/:id" element={<ExercisePage />} /> */}
       </Routes>
     </BrowserRouter>
   );
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,66 +1,82 @@
 import OpenAI from "openai";
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function MainPage() {
+  const [randomQuote, setRandomQuote] = useState("");
   //Advice
   const [question, setQuestion] = useState(null);
-  const [advice, setAdvice] = useState("");
+  const [advice, setAdvice] = useState([]);
 
-
+  //Get random quote of the day
+  const getRandomQuote = async () => {
+    try {
+      const response = await axios.get("https://stoic.tekloon.net/stoic-quote");
+      console.log(response);
+      setRandomQuote(response.data);
+    } catch (error) {}
+    // console.log(randomQuote);
+  };
 
   //Get advice from ChatGPT
   const getAdvice = async (e) => {
     e.preventDefault();
     console.log(question);
-    const openai = new OpenAI({
-      apiKey: "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", //process.env.REACT_APP_OPENAI_API_KEY
-      dangerouslyAllowBrowser: true,
-    });
+    try {
+      const openai = new OpenAI({
+        apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+        dangerouslyAllowBrowser: true,
+      });
 
-    const chatCompletion = await openai.chat.completions.create({
-      messages: [
-        {
-          role: "user",
-          content: `${question}.Please reply me with an array. The first array item is a relevant quote from stoic philosophy that would be a good fit answer to my situation or question. Second array item should be the quote's author. The third item is explanation of the quote and it's application to the my situation or question. Please provide some mental support.`,
-        },
-      ],
-      model: "gpt-3.5-turbo",
-    });
+      const chatCompletion = await openai.chat.completions.create({
+        messages: [
+          {
+            role: "user",
+            content: `${question}.Please reply me with an array. The first array item is a relevant quote from stoic philosophy that would be a good fit answer to my situation or question. Second array item should be the quote's author. The third item is explanation of the quote and it's application to the my situation or question. Please provide some mental support.`,
+          },
+        ],
+        model: "gpt-3.5-turbo",
+      });
 
-    console.log(chatCompletion.choices[0].message.content);
-    const returnedAdvice = JSON.parse(
-      chatCompletion.choices[0].message.content
-    ); //need to parse the response from json into a js
-    setAdvice(returnedAdvice);
+      console.log(chatCompletion.choices[0].message.content);
+      const returnedAdvice = JSON.parse(
+        chatCompletion.choices[0].message.content
+      ); //need to parse the response from json into a js
+      setAdvice(returnedAdvice);
+    } catch (error) {}
   };
 
+  useEffect(() => {
+    if (randomQuote === "") {
+      getRandomQuote();
+    }
+  }, [advice]); //dependency list
 
-
-  //useEffect
-  useEffect(() => {}, [advice]); //dependency list
-
-  // if (!answer) {
-  //   return <div>Loading page...</div>;
-  // }
+  if (randomQuote === "") {
+    return <div>Loading page...</div>;
+  }
 
   return (
     <div className="App">
       <section>
-      <form onSubmit={getAdvice}>
-        <input
-          type="text"
-          onChange={(e) => setQuestion(e.target.value)}
-        ></input>
-        <button type="submit">ask</button>
-      </form>
-      <div>
-        <h2>Answer</h2>
-        <h3>"{advice[0]}"</h3>
-        <p>-{advice[1]}</p>
-        <p>{advice[2]}</p>
-      </div>
+        <h2>"{randomQuote.quote}"</h2>
+        <p>-{randomQuote.author}</p>
       </section>
-
+      <section>
+        <form onSubmit={getAdvice}>
+          <input
+            type="text"
+            onChange={(e) => setQuestion(e.target.value)}
+          ></input>
+          <button type="submit">ask</button>
+        </form>
+        <div>
+          <h2>Answer</h2>
+          <h3>"{advice[0]}"</h3>
+          <p>-{advice[1]}</p>
+          <p>{advice[2]}</p>
+        </div>
+      </section>
     </div>
   );
 }
