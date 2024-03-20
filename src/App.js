3e6d409dae124a03e975656d3d7b1e54a7144a49
tatianaTun <tatianatun92@gmail.com>
2024-03-20 11:43:59 -0700

    Added routing to the app.js for main, exercises and exercise pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-
-import './App.css';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import MainPage from "./pages/MainPage/MainPage";
+import ExercisePage from "./pages/ExercisePage/ExercisePage";
 
 function App() {
-  
+
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        {/* <Route path="/exercises/" element={<ExercisesPage />} /> */}
+        <Route path="/exercise/:id" element={<ExercisePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
