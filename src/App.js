31437ef15a16851a02f8aeee541efb4d9d77e6ad
tatianaTun <tatianatun92@gmail.com>
2024-03-25 09:54:29 -0700

    Drafted challenges page, added basic styling, connected to routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import MainPage from "./pages/MainPage/MainPage";
 import ExercisePage from "./pages/ExercisePage/ExercisePage";
 import ExercisesPage from "./pages/ExercisesPage/ExercisesPage";
+import ChallengesPage from "./pages/ChallengesPage/ChallengesPage";
 import Header from "./components/Header/Header";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/exercises/" element={<ExercisesPage />} />
+        <Route path="/challenges/" element={<ChallengesPage />} />
         <Route path="/exercises/:exerciseId" element={<ExercisePage />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/ChallengesPage/ChallengesPage.js
@@ -0,0 +1,57 @@
+import "./ChallengesPage.scss";
+import { useState, useEffect } from "react";
+import { baseURL } from "../../consts";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function ChallengesPage() {
+  const [challenges, setChallenges] = useState(null);
+
+  const getChallenges = async () => {
+    try {
+      const requestUrl = `${baseURL}/challenges`;
+      const result = await axios.get(requestUrl);
+      const fetchedChallenges = result.data;
+      console.log(fetchedChallenges);
+      setChallenges(fetchedChallenges);
+      //   if (!exercises) {
+      //     setExercises(fetchedExercises);
+      //   }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getChallenges();
+  }, []);
+
+  if (!challenges) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <section className="challenges">
+      <div>
+        <h1>Challenges</h1>
+        <p>Short text about the benefits of challenging</p>
+      </div>
+      <div className="challenges__container">
+        <h2>Challenges List</h2>
+        {challenges.map((challenge) => (
+          <article  key={challenge.id} className="challenges__challenge-card">
+            <h3 className="challenges__challenge-name">{challenge.name}</h3>
+            <Link
+              to={`/challenges/${challenges.id}`}
+              className="challenges__challenge-link"
+            >
+              START
+            </Link>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default ChallengesPage;
