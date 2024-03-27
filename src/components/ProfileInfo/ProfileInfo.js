a78253e75931a1371bace6f8884634ea3328b5d0
tatianaTun <tatianatun92@gmail.com>
2024-03-27 13:02:03 -0700

    Created a component for profile information and moved it from sidebar to the output side of the profile page

---
+++ src/components/ProfileInfo/ProfileInfo.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { baseURL } from "../../consts";
+import "./ProfileInfo.scss";
+import { useNavigate } from "react-router-dom";
+
+function ProfileInfo({userInfo, level}) {
+    const navigate = useNavigate();
+
+    //
+
+
+// if(!userInfo) {
+//     return <p>Loading...</p>
+// }
+
+  return (
+    <>
+      {" "}
+      <div className="profile__user-info">
+        <div className="profile__header-container"><h3>Welcome, {userInfo.name}</h3>
+        <button
+         className="profile__logout-button"
+          onClick={() => {
+            sessionStorage.removeItem("token");
+            navigate("/");
+          }}
+        >
+          Log out
+        </button></div>
+        <p>{level}</p>
+        <p>Accomplishments: ___</p>
+      </div>
+    </>
+  );
+}
+
+export default ProfileInfo;
--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -6,16 +6,30 @@ import "./ProfilePage.scss";
 import journalEntries from "../../data/journalEntries.json";
 
 import AddEntry from "../../components/AddEntry/AddEntry";
+import ProfileInfo from "../../components/ProfileInfo/ProfileInfo";
+const profileUrl = `${baseURL}/profile`;
 
 function ProfilePage() {
   const [entries, setEntries] = useState([]);
   const [selectedEntry, setSelectedEntry] = useState(null);
   const [addEntry, setAddEntry] = useState(false);
-  const navigate = useNavigate();
-  const profileUrl = `${baseURL}/profile`;
-
   const [isLoading, setIsLoading] = useState(true);
   const [userInfo, setUserInfo] = useState({});
+  
+  const points = 22;
+  const levels = [
+    "Level 1 : Novice Navigator",
+    "Level 2 : Aspiring Apprentice",
+    "Level 3 : Virtue Voyager",
+    "Level 4 : Resilience Rookie",
+    "Level 5 : Tranquility Traveler",
+    "Level 6 : Wisdom Warrior",
+    "Level 7 : Equanimity Explorer",
+    "Level 8 : Fortitude Philosopher",
+    "Level 9 : Sage Seeker",
+    "Level 10 : Stoic Sage",
+  ];
+  const [level, setLevel] = useState(null);
 
   const fetchData = async () => {
     try {
@@ -38,6 +52,27 @@ function ProfilePage() {
   useEffect(() => {
     fetchData();
     setEntries(journalEntries);
+    if (0 < points && points < 5) {
+      setLevel(levels[0]);
+    } else if (4 < points && points < 10) {
+      setLevel(levels[1]);
+    } else if (9 < points && points < 15) {
+      setLevel(levels[2]);
+    } else if (14 < points && points < 20) {
+      setLevel(levels[3]);
+    } else if (19 < points && points < 25) {
+      setLevel(levels[4]);
+    } else if (24 < points && points < 30) {
+      setLevel(levels[5]);
+    } else if (29 < points && points < 35) {
+      setLevel(levels[6]);
+    } else if (34 < points && points < 40) {
+      setLevel(levels[7]);
+    } else if (39 < points && points < 45) {
+      setLevel(levels[8]);
+    } else if (44 < points) {
+      setLevel(levels[9]);
+    }
   }, []);
 
   //////////all the journal related functions
@@ -48,38 +83,29 @@ function ProfilePage() {
 
   const addEntryHandler = () => {
     setSelectedEntry(null);
-    setAddEntry(true)
+    setAddEntry(true);
   };
-
 
   return isLoading ? (
     <p>Loading...</p>
   ) : (
     <div className="profile">
       <section className="profile__sidebar">
-        <div className="profile__user-info">
-          <h3>Welcome, {userInfo.name}</h3>
-          <p>Level: ___</p>
-          <p>Accomplishments: ___</p>
-          <button
-            onClick={() => {
-              sessionStorage.removeItem("token");
-              navigate("/");
-            }}
-          >
-            Logout
-          </button>
-        </div>
         <div className="profile__journal">
           <h3>Journal</h3>
-          <button onClick={addEntryHandler}>add</button>
+          <button className="profile__add-button" onClick={addEntryHandler}>
+            add
+          </button>
           <div>
             <h4>All entries</h4>
             <ul>
               {entries.map((entry) => (
                 <li key={entry.id}>
                   <p>{entry.date}</p>
-                  <button  className="profile__entry-button" onClick={() => openEntryHandler(entry)}>
+                  <button
+                    className="profile__entry-button"
+                    onClick={() => openEntryHandler(entry)}
+                  >
                     {entry.title}
                   </button>
                 </li>
@@ -89,7 +115,10 @@ function ProfilePage() {
         </div>
       </section>
       <section className="profile__journal-output">
-      {addEntry && (<AddEntry />)}
+        {!addEntry && !selectedEntry && (
+          <ProfileInfo userInfo={userInfo} level={level} />
+        )}
+        {addEntry && <AddEntry />}
         {selectedEntry && (
           <div>
             <h3>{selectedEntry.title}</h3>
